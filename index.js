efdf627215376698daabb48b481598843e8f2cd8
Kirill <kirillnebaba@yandex.ru>
2026-09-17 21:21:11 +0700

    Add function for filter country

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { createObject } from "./storage.js";
+import { createObject, saveCountry } from "./storage.js";
+import { checkString, checkEmpty } from "./utils.js";
 
 //  Что то получаем
 
 const getSectionTeam = document.querySelector('.team')
 const getForm = getSectionTeam.querySelector('.team__createTeam')
+const getSearchCountry = getSectionTeam.querySelector('.team__searchCountry')
 
 //  Обработчики событий
 
@@ -16,7 +18,21 @@ getForm.addEventListener('submit', (e) => {
     const teamCountry = getChildrenForm[1].value
 
     createObject(teamName, teamCountry)
+    addCountryOption(teamCountry)
 
     getChildrenForm[0].value = ''
     getChildrenForm[1].value = ''
 })
+
+function addCountryOption(country) {
+    checkEmpty(country)
+    checkString(country)
+    const checkCountry = saveCountry(country)
+
+    if (!checkCountry) {
+        return
+    }
+
+    const getSelect = getSearchCountry.querySelector('select')
+    getSelect.innerHTML += `<option value='${country}'>${country}</option>`
+}
